be7eb8a05daf1f7d949bdb07551c39c5594bc65e
Fhree <luisgvillalonga@gmail.com>
2024-03-13 11:27:31 +0100

    1º commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:"", loadComponent: () => import('./tryThings/pages/home-page/home-page.component').then(c => c.HomePageComponent)
+  },
+  {
+    path:"invertOrder", loadComponent: () => import('./tryThings/pages/invert-order/invert-order.component').then(c => c.InvertOrderComponent)
+  }
+];
---
+++ src/app/tryThings/pages/invert-order/invert-order.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tt-invert-order',
+  standalone: true,
+  imports: [],
+  templateUrl: './invert-order.component.html',
+  styleUrl: './invert-order.component.css'
+})
+export class InvertOrderComponent {
+
+}
