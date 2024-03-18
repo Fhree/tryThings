01c890dcf8e6b8169f2b7c3da927616757056638
Luis Garcia Villalonga <lgarciav@hiberus.com>
2024-03-18 19:28:02 +0100

    Add short page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path:"", loadComponent: () => import('./tryThings/pages/home-page/home-page.component').then(c => c.HomePageComponent)
+    path:"",
+    loadComponent: () => import('./tryThings/pages/home-page/home-page.component').then(c => c.HomePageComponent)
   },
   {
-    path:"invertOrder", loadComponent: () => import('./tryThings/pages/invert-order/invert-order.component').then(c => c.InvertOrderComponent)
+    path:"invertOrder", 
+    loadComponent: () => import('./tryThings/pages/invert-order/invert-order.component').then(c => c.InvertOrderComponent)
+  },
+  {
+    path:"short", 
+    loadComponent: () => import('./tryThings/pages/short-page/short-page.component').then(c => c.ShortPageComponent)
   }
 ];
---
+++ src/app/tryThings/pages/short-page/short-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-short-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './short-page.component.html',
+  styleUrl: './short-page.component.css'
+})
+export class ShortPageComponent {
+
+}
